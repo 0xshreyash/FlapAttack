3ff0200b94b1b86c1731b686d028cb331d949b52
ShreyashPatodia <patodiashreyash32@gmail.com>
2017-03-05 14:11:32 +1100

    Basic controls update

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
+	public float upForce = 200f;
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+	const int LEFT = 0;
 	// Use this for initialization
 	void Start () {
+
+		rb2d = GetComponent<Rigidbody2D> (); 
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(!isDead)
+		{
+			if(Input.GetMouseButtonDown(LEFT))
+			{
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2(0, upForce)); 
+			}
+		}
 		
 	}
 }
